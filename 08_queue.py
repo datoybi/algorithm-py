849d9e249db4daa377a4a6012edcc8f9a0fef1aa
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-05-28 11:33:54 +0900

    Studied Stack

--- 08_queue.py
+++ 08_queue.py
@@ -4,9 +4,9 @@
     https://visualgo.net/en/list
 
   가장 먼저 넣은 데이터를 가장 먼저 꺼낼 수 있는 구조(호스)
-  FIFO(First In First Out), LILO(Last In Last Out)
-  Enqueue: 큐에 데이터를 넣는 기능
-  Dequeue: 큐에 데이터를 꺼내는 기능
+    FIFO(First In First Out), LILO(Last In Last Out)
+    Enqueue: 큐에 데이터를 넣는 기능
+    Dequeue: 큐에 데이터를 꺼내는 기능
 
   어디에 큐가 많이 쓰일까?
     멀티 테스킹을 위한 프로세스 스케쥴링 방식을 구현하기 위해 많이 사용됨 (운영체제 참조), 스케쥴링 방식을 함께 이해해두는 것이 좋음
