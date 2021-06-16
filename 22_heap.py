40402cb9c8bb777be196011b2ff294cf4a5fa52d
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-06-16 09:59:38 +0900

    add comment insert heap

--- 22_heap.py
+++ 22_heap.py
@@ -5,18 +5,19 @@
             부모 노드 인덱스 번호(parent node's index) = 자식 노드 인덱스 번호 (child node's index) //2
             왼쪽 자식 노드 인덱스 번호 (left child node's index) = 부모 노드 인덱스 번호 (parent node's index) *2
             오른쪽 자식 노드 인덱스 번호 (right child node's index) = 부모 노드 인덱스 번호 (parent node's index) *2+1
+
 '''
 # 힙의 데이터 삽입
 #  1. 완전 이진트리처럼 데이터 채우기 2. 부모노드와 비교를 해서 부모노드보다 작을 떄 까지 바꿔주는 작업
 class Heap:
     #완전 이진트리처럼 데이터 채우기
     def __init__(self, data):
-        self.heap_array = list()
-        self.heap_array.append(None)
+        self.heap_array = list() 
+        self.heap_array.append(None) # 맨 앞 노드는 0으로 노드가 1부터 시작하는게 더 보기 편하기 때문
         self.heap_array.append(data)
 
-    def move_up(self, inserted_idx):
-        if inserted_idx <= 1:
+    def move_up(self, inserted_idx):  # 루트노드와 비교하여 바꿔야되는지 판단해주는 메서드
+        if inserted_idx <=1: # inserted_idx가 root node 일 때
             return False
         parent_idx = inserted_idx // 2
         if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
@@ -24,19 +25,19 @@ class Heap:
         else:
             return False
 
-    def insert(self, data):
+    def insert(self, data): # 초기화
         if len(self.heap_array) == 0:
             self.heap_array.append(None)
             self.heap_array.append(data)
             return True
         self.heap_array.append(data)
     #  부모노드와 비교를 해서 부모노드보다 작을 떄 까지 바꿔주는 작업  
-        inserted_idx = len(self.heap_array)-1
+        inserted_idx = len(self.heap_array)-1 # 배열[0]번째에 None을 넣었으니 len에서 -1을 해준다
 
         while self.move_up(inserted_idx):
-            parent_idx = inserted_idx//2
-            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx] # swap
-            inserted_idx = parent_idx
+            parent_idx = inserted_idx//2 # 부모노드 인덱스 번호
+            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx] # swap a,b = b,a
+            inserted_idx = parent_idx # 인덱스 번호도 바꿔주기
 
         return True
 
