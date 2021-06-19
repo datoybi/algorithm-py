0f0d1e2501f2fd05a7fcdb843e4491cde0aaa841
dasom yun <dsy0302@gmail.com>
2021-06-19 17:12:11 +0900

    updated recursive call

--- theory/algorithm/07_recursivecall.py
+++ theory/algorithm/07_recursivecall.py
@@ -1,3 +1,11 @@
+'''
+    참고 - 리스트 슬라이싱
+    string = 'Dave'
+    string[-1] --> e
+    string[0] --> D
+    string[1:-1] --> av
+    string[:-1] --> Dav
+'''
 # 재귀용법을 활용한 프로그래밍 연습
 
 # 다음 함수를 재귀 함수를 활용해서 완성해서 1부터 num까지의 곱이 출력되게 만드세요
@@ -37,7 +45,7 @@ def sum_list(data): # 반복문 사용
 def sum_list2(data):
     if len(data) <= 1:
         return data[0]
-    return data[0] + sum_list(data[1:])
+    return data[0] + sum_list(data[1:]) #부터 쭉 합을 더하기
     
 
 import random
@@ -52,3 +60,27 @@ print(sum_list2(data_list))
 # 회문(palindrome)은 순서를 거꾸로 읽어도 제대로 읽은 것과 같은 단어와 문장을 의미함 
 # 회문을 판별할 수 있는 함수를 리스트 슬라이싱을 활용해서 만드세요
 
+# 내가 만든 답
+'''
+def palindrome(str):
+    if str[-1] != str[0]:
+        return False
+    elif str[:-1] == str[0:-1]:
+        #print(str[:-1])
+        #print(str[0:-1])
+        return True
+
+
+print(palindrome("level"))
+
+'''
+
+def palindrome(string):
+    if len(string) <= 1:
+        return True
+    if string[0] == string[-1]:
+        return palindrome(string[1:-1])
+    else:
+        return False
+
+print(palindrome("level"))
