4e0e75e05324ca45b44f18b700dc224cfd789ab6
dasom yun <dsy0302@gmail.com>
2021-06-27 18:05:51 +0900

    reviewed

--- theory/algorithm/practice.py
+++ theory/algorithm/practice.py
@@ -1,21 +1,39 @@
 
-data_list = [(10,10),(15,12),(20,10),(25,8),(30,5)]
 
-def calculate(data_list, capacity):
-    data_list = sorted(data_list, key=lambda x:x[1]/x[0], reverse=True)
+'''
+    탐욕 알고리즘의 예 
+        문제1. 동전 문제 
+            지불해야 하는 값이 4720원 일때 1원 50원 100원, 500원 동전으로 동전의 수가 가장 적게 지불하시오.
+            가장 큰 동전부터 최대한 지불해야 하는 값을 채우는 방식으로 구현 가능
+            탐욕 알고리즘으로 매순간 최적이라고 생각되는 경우를 선택하면 됨
+            
+    참고
+        coin_list.sort(reverse=True) -> 내림차순으로 정렬
+        data_list = sorted(data_list, key=lambda x:x[1]/x[0], reverse=True)
+
+'''
+
+coin_list = [100, 500, 50, 1]
+# print(coin_list)
+coin_list.sort(reverse=True)
+# print(coin_list)
+
+def calculate(coin_list, money):
     total = 0
-    details = list()
+    result = list()
+    coin_list.sort(reverse=True)
 
-    for data in data_list:
-        if capacity - data[0] >= 0:
-            capacity -= data[0]
-            total += data[1]
-            details.append([data[0], data[1], 1])
-        else:
-            fraction = capacity / data[0]
-            total += data[1] * fraction
-            details.append([data[0], data[1], fraction])
-            break
-    return total, details
+    for coin in coin_list:
+        coin_num = money // coin
+        total += coin_num
+        money -= coin_num * coin
 
-print(calculate(data_list, 30))
+        result.append([coin, coin_num])
+
+    return total, result
+
+            
+
+print(calculate(coin_list,4561))
+
+
