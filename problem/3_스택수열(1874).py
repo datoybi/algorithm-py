c00818f1f9304ffd8705bfb7993348c1f0e09e95
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-07-30 14:33:42 +0900

    solve 스택수열

--- problem/3_스택수열(1874).py
+++ problem/3_스택수열(1874).py
@@ -4,31 +4,74 @@
     유형 : 스택, 그리디
     시간 : 30분
     느낀점 : 문제를 이해 못하겠음ㅜ, 문제 해석 잠깐 보고 이해함
+            변수 많이 만들 필요도 없고 뒤에서 무슨 일 일어나는지도 안 중요함 -> '중요한건 주어진 절차에 의거하여 결과값이 잘 나오는지' 이다
 """
 
+# count = int(input())
+# inputArr = []
+# makingList = []
+# stack = []
 
+# for i in range(0, count):
+#     inputArr.append(int(input()))
 
-count = int(input())
-inputArr = []
-makingList = []
-stack = []
+# # print("\n")
+# # print(inputArr[0])
+# # del inputArr[0]
+# # print(inputArr[0])
 
-for i in range(0, count):
-    inputArr.append(int(input()))
+# for j in range(1, 100): # 1~100까지 스텍 만들기
+#     print(inputArr[0]) # 4
+#     stack.append(j) # 1 2 3 4
 
-for k in range(0, count):
-    for j in range(1, 100):
-        print(inputArr[k])
-        stack.append(j)
+#     if inputArr[0] != stack[j]:
+#         # print(inputArr[k])
+#         print('+')
 
-        if inputArr[k] != stack[j]:
-            # print(inputArr[k])
-            print('+')
-            break;
-
-        elif inputArr[k] == j:
-            print('-')
-            del stack[j]
-            continue;
+#     elif inputArr[0] == j:
+#         print('-')
+#         del stack[j]
+#         del inputArr[0]
     
-# print(inputArr)
+# # print(inputArr)
+
+
+# n = int(input())
+# count = 1
+# result = []
+# stack = []
+
+# for i in range(1, n+1): # 데이터 개수만큼 반복
+#     data = int(input())
+#     while count <= data: # 입력 받은 데이터에 도달할 때까지 삽입
+#         stack.append(count)
+#         count += 1
+#         result.append('+')
+#     if stack[-1] == data: # 스택의 최상위 원소가 데이터와 같을 때 출력
+#         stack.pop()
+#         result.append('-')
+#     else: # 불가능한 경우
+#         print('NO')
+#         exit(0)
+
+# print('\n'.join(result)) # 가능한 경우
+
+n = int(input())
+result = []
+stack = []
+count = 1
+
+for i in range (1, n+1):
+    data = int(input())
+    while count <= data:
+        stack.append(count)
+        count += 1
+        result.append('+')
+    if stack[-1] == data:
+        stack.pop()
+        result.append('-')
+    else :
+        print('NO')
+        exit(0)
+
+print('\n'.join(result))
