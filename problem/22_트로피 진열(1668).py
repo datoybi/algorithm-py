00f5045c5f2399f1c37ecece5848ad14f2ade21f
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-09-09 17:55:24 +0900

    solving problem

--- problem/22_트로피 진열(1668).py
+++ problem/22_트로피 진열(1668).py
@@ -3,9 +3,21 @@
     난이도 : 하
     유형 : 탐색
     시간 : 20분
-
+    문제 풀이 핵심 아이디어 
+        선반 위에 올려져 있는 트로피들에 대하여 왼쪽에서, 오른쪽에서 봤을 때 보이는 
+        트로피의 수를 각각 구합니다.
+        트로피의 개수 N이 최대 50이므로 단순히 구현하면 됩니다.
 
 '''
+
+def ascending(array):
+    result = 1
+    now = array[0]
+    for i in range(1, len(array)):
+        if now < array[i]:
+            result += 1
+            now = array[i]
+    return result;
 
 n = int(input())
 array = list()
@@ -13,24 +25,6 @@ array = list()
 for _ in range(n):
     array.append(int(input()))
 
-# print(array)
-
-left = 1
-right = 1
-max = array[0]
-# print(left , right)
-
-for i in range(0, len(array)-1):
-    if max < array[i+1]:
-        left += 1
-
-for i in range(0, len(array)-1):
-    if max > array[i-1]:
-        right += 1
-# for i in range(len(array)-1, 0, -1):
-#     if array[i] <= array[i-1]:
-#         right += 1
-    # print(i, i-1)
-
-print(left)
-print(right)
+print(ascending(array))
+array.reverse()
+print(ascending(array))
