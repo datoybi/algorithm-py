476741efb46bd4a8ef49f5a1810f445a2f88aad9
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-09-10 17:15:27 +0900

    let's study tree, and heap

--- problem/practice.py
+++ problem/practice.py
@@ -1,17 +1,52 @@
-books = {}
-n = int(input())
-arranged = list()
 
-for _ in range(n):
-    book = input()
-    if book not in books:
-        books[book] = 1
-    else:
-        books[book] += 1
-    target = max(books.values())
+# 노드 클래스 만들기
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# 이진 탐색 트리에 데이터 넣기 (작은값 왼쪽, 큰값 오른쪽)
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head # root node
     
-    for book, number in books.items():
-        if number == target:
-            arranged.append(book)
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value: # current_node가 더 크다면 
+                if self.current_node.left != None: # if is there any child node,
+                      self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else: # current_node가 더 작다면
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else: 
+                    self.current_node.right = Node(value)
+                    break
+    
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False
 
-print(sorted(arranged)[0])
+
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
+BST.insert(3)
+BST.insert(0)
+BST.insert(4)
+BST.insert(8)
+
+print(BST.search(8))
+print(BST.search(1000))
+
