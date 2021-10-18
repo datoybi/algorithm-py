f4a83ec1d4a0f1b446b713a48177d96fe4526c21
dasom yun <dsy0302@gmail.com>
2021-10-18 22:22:27 +0900

    today's work

--- 완전탐색/6_일곱난쟁이(2309).py
+++ 완전탐색/6_일곱난쟁이(2309).py
@@ -1,27 +1,18 @@
 # 키 , 100미만 자연수 키 , 모두 다름 , 여러가지인 경우 아무거나 출력 
 # 일곱 난쟁이의 키의 합 = 100 
 # 힌트를 얻었음 ! 2개뺴고 다 더한값이 100이라고 가정 하기
-
-
 lst = list()
-test = list()
-add = 0
-selected
 
 for _ in range(0,9):
     lst.append(int(input()))
 lst.sort()
 
-    print(sum(lst[0:7]))
-
-# for i in lst:   
-    
-    # if sum(lst[0:7]) == 100:
-    # print(sum(lst[0:7]))
-    # add += i
-    # if add < 100:
-    #     add += i
-    # elif add > 100:
-    #     add += i
-    # elif add == 100:
-    #     print(test)
+for i in range(8, 0, -1): # 8-1
+  for j in range(7,-1,-1): # 7-0
+    result = sum(lst[0:]) - lst[i] - lst[j]
+    #print(result)
+    if result == 100:
+      del lst[i], lst[j]
+      for z in lst:
+        print(z)
+      exit(0)
