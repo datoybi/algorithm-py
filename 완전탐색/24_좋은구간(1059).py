8442463c867bc0a5537aa61b7fcd396d6510c093
dasom yun <dsy0302@gmail.com>
2021-10-25 22:54:57 +0900

    today's work

--- 완전탐색/24_좋은구간(1059).py
+++ 완전탐색/24_좋은구간(1059).py
@@ -1,29 +1,77 @@
 '''
-    집합 S의 크기 L
-    집합에 포함된 정수들
-    n
+    맞았습니다!
+    -----
+    아오 왜자꾸 틀리는지 모르겠다 
+    반례 다 적용해봐도 안된다..
+    왜 틀리는지 잘 모르겠따 머리아프당.. 
+    --- 
+    이랬었는데 결국 반례 찾아서 맞았다
+    반례
 
-    
-    
-    1 <= L <= 50
-    S에는 중복되는 정수가 없음
-    S에 포함된 정수는 1<= 정수 <= 10000
+    2
 
-    1<=n<= 집합 S에서 가장 큰 정수
+    3 7
 
+    2
 
-    조건들
-    0<A<B
-    A~B 안에 집합의 원소가 속하지 않는다
-    n이 범위안에 포함 되면 됨 
-
+    요 반례를 적용하니 되었다..
 
 '''
-
 L = int(input())
 S = list(map(int, input().split()))
-N = int(input())
+S.sort()
+n = int(input())
+
+max_num, min_num, count = 1000, 0, 0
+
+for i in range(0, L): 
+    if n in S: # 집합 안에 n이 있는 경우 
+        print(0) 
+        exit(0)
+    if n <= S[i]: 
+        if i == 0: # S[0]이 n보다 큰 경우 -> 그걸 max_num으로
+            max_num = S[i]
+        else: # 그렇지 않은 경우 (중간인 경우)
+            max_num = S[i] 
+            min_num = S[i-1]
+        break
+
+if L == 1: # 만약 집합의 원소가 1개인 경우
+    max_num = S[i]
+# print(min_num, max_num)
+
+for i in range(min_num+1, n+1):
+    # print(i)
+    for j in range(i+1, max_num):
+        if n <= j :
+            # print(i, j)
+            count+=1
+        
+print(count)
 
 
+# 수학적 접근
+# import sys
+# input=sys.stdin.readline
 
+# l=int(input())
+# a=list(map(int,input().split()))
+# a=[0]+a
+# a.sort()
+# n=int(input())
+
+# left=0
+# right=0
+# if n in a:
+#     print(0)
+#     sys.exit()
+
+# for x in a:
+#     if n>x:
+#         left=x+1
+#     else:
+#         right=x-1
+#         break
+# print(left,right)
+# print(right-left+(right-n)*(n-left)) # 이 식을 어캐 생각해낸거지..?
 
---
+++ 완전탐색/25_단어나누기(1251).py
@@ -0,0 +1,7 @@
+str = 'bometil'
+
+for i in str:
+    for j in str:
+        for z in str:
+            print(i,j,z)
+
