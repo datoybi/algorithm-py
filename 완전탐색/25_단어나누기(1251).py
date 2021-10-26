d285a7a88d9890b386cf43f7433bb97f14a0128d
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-10-26 17:52:29 +0900

    today''s work

--- 완전탐색/25_단어나누기(1251).py
+++ 완전탐색/25_단어나누기(1251).py
@@ -1,7 +1,16 @@
-str = 'bometil'
+'''
+맞았습니다!
+'''
+str, lst = input(), list()
 
-for i in str:
-    for j in str:
-        for z in str:
-            print(i,j,z)
+for i in range (1, len(str)-2):
+  for j in range (i+1, len(str)):
+    # print(str[0:i], str[i:j], str[j:])
+    str1, str2, str3 = str[0:i], str[i:j], str[j:]
+    real = str1[::-1] + str2[::-1] + str3[::-1]
+    # print(ord(real))
 
+    lst.append(real)
+lst.sort()
+
+print(lst[0])
