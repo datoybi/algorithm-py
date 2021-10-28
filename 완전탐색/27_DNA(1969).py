1ae7d7ec42f77e461f94a3e6176c4832ab0ba309
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-10-28 18:02:46 +0900

    today's work

--- 완전탐색/27_DNA(1969).py
+++ 완전탐색/27_DNA(1969).py
@@ -5,27 +5,80 @@ count = list()
 str = ''
 cnt = 0
 distance = 0
+temp = list()
+temp2 = list()
 
 
-for i in range(0, len(lst)): #0~4
-    cnt = 0
-    str = lst[i]
+for i in range(0, 8): # 0-4
+    tmp = list()
+    tmp_str = ''
     for j in range(0, len(lst)):
-        # print(lst[i])
-        tmp1 = list(lst[i])
-        tmp2 = list(lst[j])
-        # print(tmp1, tmp2)
-        for z in range(0,8):
-            # print(tmp1[z], tmp2[z])
-            if tmp1[z] != tmp2[z]:
-                cnt += 1
-                print(tmp1[z], tmp2[z], z)
-                print(cnt)
+        tmp += list(lst[j][i])
+        tmp_str += lst[j][i]
+        # print(tmp)
+    temp.append(tmp)
+    temp2.append(tmp_str)
 
-    count.append((str, cnt))
-    print('\n')
+print(temp)
+# print(temp2)
 
-print(count)
+# print(temp[0].count('T'))
+
+max_value = ''
+max_count = 0
+result_lst = list()
+
+for i in range(len(temp)):
+    print(i)
+    max_value = ''
+    max_count = 0
+    for j in range(len(temp[0])):
+        # print(temp[i][j])
+        tmp_cnt = temp[i].count(temp[i][j])
+        
+        # tmp_value = ''
+        # print(temp[i].count(j[0]))
+        print(temp[i] ,temp[i][j])
+        
+        # print("tmp_cnt : " , tmp_cnt , " , i[j] : " , i[j])
+
+        if max_count < tmp_cnt: 
+            # print(tmp_cnt , j)
+            max_count = tmp_cnt
+            max_value = temp[i][j]
+
+        elif max_count == tmp_cnt:
+            if max_value > temp[i][j]:
+                print(max_value , temp[i][j])
+                max_value = temp[i][j]
+
+        print("max_count : " , max_count , " , max_value : " , max_value)
+
+    result_lst.append(max_value)
+        # tmp_cnt = temp[i].count(j)   
+        # print(tmp_cnt)
+
+
+print(result_lst)
+# for i in range(0, len(lst)): #0~4
+#     cnt = 0
+#     str = lst[i]
+#     for j in range(0, len(lst)):
+#         # print(lst[i])
+#         tmp1 = list(lst[i])
+#         tmp2 = list(lst[j])
+#         # print(tmp1, tmp2)
+#         for z in range(0,8):
+#             # print(tmp1[z], tmp2[z])
+#             if tmp1[z] != tmp2[z]:
+#                 cnt += 1
+#                 print(tmp1[z], tmp2[z], z)
+#                 print(cnt)
+
+#     count.append((str, cnt))
+#     print('\n')
+
+# print(count)
         
          
 
