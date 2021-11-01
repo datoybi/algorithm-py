a1a3777cb5ab6c6ce66b184b74d347651ce542b9
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-11-01 09:53:00 +0900

    today's work

--- 완전탐색/30_숫자 야구(2503).py
+++ 완전탐색/30_숫자 야구(2503).py
@@ -9,7 +9,37 @@
     356 1 0
     327 2 0
     489 0 1
+
+    2
 '''
 
 n_lst = [123, 356, 327, 489]
 s_lst = [[1, 1],[1, 0],[2, 0],[0, 1]]
+whole_lst = list()
+
+num = list('429')
+answer = list('324')
+
+print(num, answer)
+
+for i in range(100,501):
+    whole_lst.append(i)
+
+    for j in range(3):
+        if num[j] == answer[j]: # 스트라이크인지 ?
+            print("스트라이크")
+            break
+
+        if num[j] in answer : # 볼인지 ?
+            print("뽈")
+            
+print(whole_lst)
+
+
+# for i in range(3):
+#     if num[i] == answer[i]: # 스트라이크인지 ?
+#         print("스트라이크")
+#         break
+
+#     if num[i] in answer : # 볼인지 ?
+#         print("뽈")
