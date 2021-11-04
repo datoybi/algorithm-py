6f808274b3b0d6f4063519b097f76a18d1fa4ebd
dasom yun <dsy0302@gmail.com>
2021-11-04 22:10:35 +0900

    today's work

---
+++ 완전탐색/33_카드 놓기(5568).py
@@ -0,0 +1,45 @@
+n, k = int(input()), int(input())
+n_lst = list()
+
+for _ in range(n):
+    n_lst.append(input())
+
+result_set = set()
+
+'''
+4
+3
+1
+2
+12
+1
+'''
+
+if n > 10 or n < 4 :
+    exit(0)
+
+for i in range(len(n_lst)):
+    for j in range(len(n_lst)):
+        if i==j :
+            continue
+        elif k == 3: 
+            for z in range(len(n_lst)):
+                if j==z or z==i:
+                    continue
+                elif k == 4:
+                     for a in range(len(n_lst)):
+                        if z==a or j==z or z==i:
+                            continue
+                        else:
+                            
+                            result_set.add(n_lst[i] + n_lst[j] + n_lst[z] + n_lst[a])
+                else:
+                    print(i, j, z)
+                    # print(n_lst[i] , n_lst[j] , n_lst[z])
+                    result_set.add(n_lst[i] + n_lst[j] + n_lst[z])
+        else:
+            # print(n_lst[i] , n_lst[j])
+            result_set.add(n_lst[i] + n_lst[j])
+                      
+print(result_set)
+print(len(result_set))
--- 완전탐색/test.py
+++ 완전탐색/test.py
@@ -1,49 +1,13 @@
-def check(one, two, three, s, b):
-    for i in range(100,1000):
-        ns, nb = 0, 0
-        if(res[i]):
-            n_one = int(i/100)
-            n_two = int((i%100)/10)
-            n_three = int((i%100)%10)
-            
-            if(one == n_one):
-                ns += 1
-            if(two == n_two):
-                ns += 1
-            if(three == n_three):
-                ns += 1
-            
-            if(one != n_one and (one == n_two or one == n_three)):
-                nb += 1
-            if(two != n_two and (two == n_one or two == n_three)):
-                nb += 1
-            if(three != n_three and (three == n_one or three == n_two)):
-                nb += 1
-            
-            if(ns != s or nb != b):
-                res[i] = False
+from itertools import permutations
+import sys
+input = sys.stdin.readline
 
-N = int(input())
-res = [True for i in range(1000)]
+n,k = int(input()), int(input())
+cards = [input().rstrip() for _ in range(n)]
+res = set()
 
-for i in range(100,1000):
-    one = int(i/100)
-    two = int((i%100)/10)
-    three = int((i%100)%10)
-    if(one == two or two == three or three == one):
-        res[i] = False
-    if(one == 0 or two == 0 or three == 0):
-        res[i] = False
+for per in permutations(cards, k):
+    res.add(''.join(per))
 
-for n in range(N):
-    num, s, b = map(int, input().split())        
-    num = str(num)
-    one, two, three = int(num[0]),int(num[1]),int(num[2])
-    check(one, two, three, s, b)
-
-cnt = 0
-for i in range(100,1000):
-    if(res[i]):
-        cnt += 1
-
-print(cnt)
+print(res)
+print(len(res))
