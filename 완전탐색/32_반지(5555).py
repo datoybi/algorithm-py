723ad050850b35cc98d448743c0df5c311b3dc51
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-11-04 13:43:05 +0900

    today's work

--- 완전탐색/32_반지(5555).py
+++ 완전탐색/32_반지(5555).py
@@ -1,35 +1,28 @@
 '''
-    
-
+맞았습니다!
 '''
-# str, N = input(), int(input())
-# str_lst = list()
+str, N = input(), int(input())
+count = 0
+str_lst = list()
 
-# for i in range(3):
-#     str_lst.append([input()])
+for i in range(N):
+    str_lst.append(input()) # 초기값 세팅
 
-str, N = 'ABCD', 3
-flag = False 
-str_lst = [['ABCDXXXXXX'], ['YYYYABCDXX'], ['DCBAZZZZZZ']]
+for item in str_lst : # str list 만큼 요소 뽑아오기
+    for i in range(0, len(item)+1):
+        tmp = item
+        j = i + len(str)
 
-# print(str_lst[0])
-for i in range(0,11):
-    tmp = str_lst[0][0]
-    j = i+len(str)
-    print('i : ' , i , ' j : ' , j)
+        if j <= len(item): # j가 LENGTH보다 작을 때        
+            if tmp[i:j] == str:
+                count += 1
+                break
 
-    if j <= len(str_lst[0][0]):
-        print(tmp[i:j]) 
+        elif j > len(tmp): # j가 length를 초과했다면   
+            s = j-len(tmp)
+    
+            if tmp[i:j]+tmp[0:s] == str: # 덧붙이기
+                count += 1
+                break
 
-    elif j > len(str_lst[0][0]):
-        s = j-len(str_lst[0][0])
-        print('s : ' , s)
-        print('hap : ' , tmp[i:j] , ' , ' ,  tmp[0:s])
-        # if s == len(str):
-        #     break
-
-        if tmp[0:s] == str:
-            break
-
-    # print(tmp[i:j])
-    # print(tmp)
+print(count)
