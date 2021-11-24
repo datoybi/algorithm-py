dcc13a0e174e639b0f194e348811b3b3bc7f146c
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-11-24 10:29:20 +0900

    today's work

--- 문자열(easy)/58_iSharp(3568).py
+++ 문자열(easy)/58_iSharp(3568).py
@@ -3,23 +3,25 @@
 import sys
 
 variable = list(map(str, sys.stdin.readline().split()))
-front = variable[0]
-lst = list()
-print(front)
+front, lst = variable[0], list()
 
 for i in range(1, len(variable)):
-    lst.append(variable[i][0:len(variable)])
+    lst.append(variable[i][0:len(variable[i])-1]) # 예쁘게 걸러서 리스트에 다시 담기
 
 check = ['&', '[]', '*']
 
-print(lst)
+for i in range(len(lst)):
+    tmp = ''
+    cnt = 0 # 변수 길이 측정
+    for j in range(len(lst[i])-1, -1, -1): # 뒤에서부터 
+        for z in range(len(check)):
+            length = len(check[z])
+            # print(lst[i][j:j+length])
+            if check[z] == lst[i][j:j+length]: # 뒤에서 부터 check len만큼 가져와 맞는지 판별
+                tmp += check[z]
+                cnt = j
 
-# ['int&', 'a*[]&,', 'b,', 'c*;']
-# for i in range(1, len(variable)):
-#     temp =  variable[0]
-#     print(variable[i][1:])
-    # print(rev[::-1])
-
-
-    # for j in range(len(check)):
-    #     if check[j] in variable[i]:
+    if cnt != 0: # 변수길이
+        print(f'{front}{tmp} {lst[i][0:cnt]};')
+    else:
+        print(f'{front}{tmp} {lst[i][0]};')
--- 완전탐색(easy)/test.py
+++ 완전탐색(easy)/test.py
@@ -6,5 +6,7 @@ int&&[]* a;
 int& b;
 int&* c;
 '''
+lst = ['a*[]&']
 
-
+print(lst[0][4:5])
+print(lst[0][len(lst[0])-1:len(lst[0])])
