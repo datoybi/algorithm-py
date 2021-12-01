dbb7f5f6be6ae2c8b21c0e586906b81488f775fc
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-12-01 18:00:43 +0900

    today's work

--- 문자열(easy)/77_단어 뒤집기 2(17413).py
+++ 문자열(easy)/77_단어 뒤집기 2(17413).py
@@ -1,63 +1,106 @@
-# 다시 천천히 해보기
+# 아오 쉽게 안풀리니까 이따 풀어보긩
+
+
 import sys
 
 sentence = list(map(str, sys.stdin.readline().rstrip()))
-# ['baekjoon', 'online', 'judge']
-# ['<open>tag<close>']
-print(sentence)
-
+print(sentence) # ['<', 'a', 'b', ' ', 'c', 'd', '>', 'e', 'f', ' ', 'g', 'h', '<', 'i', 'j', ' ', 'k', 'l', '>']
 result = list()
-arrow = False
-tword = list()
+flag = False
+word = ''
 
 for i in range(len(sentence)):
-    # print(sentence[i])
-    print('result : ' , result)
-    print('sentence : ' , sentence[i])
     if sentence[i] == '<':
-        if len(result) != 0:
-            print('ues')
-            word = ''
-            new = list()            
-            tmp = list()
-            for z in range(len(tword)):
-                if tword[z] == ' ':
-                    # new.append(tmp[::-1])
-                    for i in tmp:
-                        new.append(i)
-                    new.append(' ')
-                    tmp = []
-                else:
-                    tmp.insert(0, tword[z])
-                # new.insert(0, tword[z])
-                # result.append(tword[z])
-            if tmp:
-                for i in tmp:
-                    new.append(i)
-            print('new!! ' ,new)
-            for k in new :
-                result.append(k)
-        arrow = True
-    elif sentence[i] == '>':
+        flag = True
+
+    if sentence[i] == '>':
         result.append(sentence[i])
-        arrow = False
+        flag = False
         continue
 
-    if arrow == True:
+    if flag == True:
         result.append(sentence[i])
-        # if sentence[i] != ' ': 
-        #     tword.insert(0, sentence[i])
-        # elif sentence[i] == ' ':
-        #     print(tword)
-        #     for z in range(len(tword)):
-        #         result.append(tword[z])
-        #     result.append(' ')
-        #     tword = list()
+    
+    # if flag == False:
+    #     print(sentence[i])
+
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 다시 천천히 해보기
+
+# import sys
+
+# sentence = list(map(str, sys.stdin.readline().rstrip()))
+# # ['baekjoon', 'online', 'judge']
+# # ['<open>tag<close>']
+# print(sentence)
+
+# result = list()
+# arrow = False
+# tword = list()
+
+# for i in range(len(sentence)):
+#     # print(sentence[i])
+#     print('result : ' , result)
+#     print('sentence : ' , sentence[i])
+#     if sentence[i] == '<':
+#         if len(result) != 0:
+#             print('ues')
+#             word = ''
+#             new = list()
+#             tmp = list()
+#             for z in range(len(tword)):
+#                 if tword[z] == ' ':
+#                     # new.append(tmp[::-1])
+#                     for i in tmp:
+#                         new.append(i)
+#                     new.append(' ')
+#                     tmp = []
+#                 else:
+#                     tmp.insert(0, tword[z])
+#                 # new.insert(0, tword[z])
+#                 # result.append(tword[z])
+#                 print(tmp)
+#             if tmp:
+#                 for i in tmp:
+#                     new.append(i)
+#             print('new!! ' ,new)
+#             for k in new :
+#                 result.append(k)
+#         arrow = True
+#     elif sentence[i] == '>':
+#         result.append(sentence[i])
+#         arrow = False
+#         continue
+
+#     if arrow == True:
+#         result.append(sentence[i])
+#         # if sentence[i] != ' ': 
+#         #     tword.insert(0, sentence[i])
+#         # elif sentence[i] == ' ':
+#         #     print(tword)
+#         #     for z in range(len(tword)):
+#         #         result.append(tword[z])
+#         #     result.append(' ')
+#         #     tword = list()
             
-                # tword = list()
+#                 # tword = list()
 
-    # print(tword)
-    print(result)
+#     # print(tword)
+#     print(result)
 
-for i in result:
-    print(i, end='')
+# for i in result:
+#     print(i, end='')
--- 완전탐색(easy)/test.py
+++ 완전탐색(easy)/test.py
@@ -1,13 +1,42 @@
 '''
 baekjoon online judge
 <open>tag<close>
-<ab cd>ef gh<ij kl>
 
+<ab cd>ef gh<ij kl>
 <ab cd>fe hg<ij kl>
 
-int& a*[]&, b, c*;
 
-int&&[]* a;
-int& b;
-int&* c;
+<int><max>2147483647<long long><max>9223372036854775807
+<int><max>7463847412<long long><max>7085774586302733229
 '''
+
+
+str = ['e', 'f', ' ', 'g', 'h', '<']
+result = ''
+lst = list()
+tmp = list()
+
+for i in range(len(str)):
+    print(i)
+    print(tmp)
+
+    if str[i] == ' ':
+        print('here1 : ' , tmp)
+        # lst.append(tmp)
+        for t in tmp:
+            lst.append(t)
+        lst.append(' ')
+        tmp = []  
+
+    if str[i] == '<':
+        print('here2 : ' , tmp)
+        for t in tmp:
+            lst.append(t)
+        # lst.append(tmp)
+        tmp = []   
+
+    else: 
+        tmp.insert(0, str[i])
+
+print(lst)
+
