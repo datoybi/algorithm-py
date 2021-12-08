69baf3ff76c33ad86861535dcfa5c2a19e77a0a2
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-12-08 18:03:18 +0900

    today's work

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-'''
 
+lst = ['?', 'O', 'H', 'R', 'V', 'A', 'T', 'I', 'N'] 
+last_word, last_idx = '', 1
+del lst[0]
+print(lst)
 
-'''
+lst.reverse()
+print(lst)
+
+for j in range(len(lst)):
+    for i in lst[j:]:
+        print(i, end='')
+    for i in lst[0:j:]:
+        print(i, end='')
+
---
+++ 시뮬레이션 (easy)/30_행운의 바퀴(2840).py
@@ -0,0 +1,51 @@
+'''
+    바퀴를 K번 돌림 
+    어떤 글자에서 멈추었는지 종이에 적는다 
+    종이를 바탕으로 바쿠에 적은 알파벳 알아내기 
+
+    바퀴의 칸의 수 N, 바퀴를 돌리는 횟수 k
+    화살표가 가리키는 글자가 몇번 바뀌었는지 나타내는 S, 회전을 멈추었을 때 가리키던 글자
+
+    마지막 회전에서 화살표가 가리키는 문자부터 시계방향으로 알파벳 출력 모르는 칸은 ? 행운의 바퀴가 없으면 !
+
+'''
+# 왜 런타임 indexof 에러?! 아놔
+import sys
+
+N, K = list(map(int, sys.stdin.readline().split()))
+lst = ['?' for _ in range(N+1)]
+flag = True
+now = 0
+last_word, last_idx = '', 0
+
+for j in range(K):
+    S, word = sys.stdin.readline().split()
+    now += int(S)
+    if now > N: # 돌림판의 개수를 넘어가면 
+        now -= N
+    if lst[now] == '?': 
+        lst[now] = word
+    else:
+        if lst[now] != word:
+            flag = False
+    if j == K-1:
+        last_word = word
+    # print(now, word, lst, flag)
+
+print(lst, flag)
+# 출력
+if flag == False:
+    print("!")
+else:
+    del lst[0]
+    lst.reverse()
+    for i in range(N):
+        if lst[i] == last_word:
+            last_idx = i
+        if lst[i] != '?' and lst.count(lst[i]) != 1:
+            print('!')
+            exit(0)
+    for i in lst[last_idx:]:
+        print(i, end='')
+    for i in lst[0:last_idx:]:
+        print(i, end='')
