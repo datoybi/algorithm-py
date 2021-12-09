8f60d9917e148cbb8fb80ff3be98e8e43cce1cf5
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2021-12-09 17:56:55 +0900

    today's work

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 
-lst = ['?', 'O', 'H', 'R', 'V', 'A', 'T', 'I', 'N'] 
-last_word, last_idx = '', 1
-del lst[0]
-print(lst)
+# lst = ['?', 'O', 'H', 'R', 'V', 'A', 'T', 'I', 'N'] 
+# last_word, last_idx = '', 1
+# del lst[0]
+# print(lst)
 
-lst.reverse()
-print(lst)
+# lst.reverse()
+# print(lst)
 
-for j in range(len(lst)):
-    for i in lst[j:]:
-        print(i, end='')
-    for i in lst[0:j:]:
-        print(i, end='')
+# for j in range(len(lst)):
+#     for i in lst[j:]:
+#         print(i, end='')
+#     for i in lst[0:j:]:
+#         print(i, end='')
 
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\__|")
