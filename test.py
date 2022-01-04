74f1abe452b748aea35b003fe6f16ed3864de09c
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-01-04 17:54:00 +0900

    today's work

--- test.py
+++ test.py
@@ -12,5 +12,28 @@ ans 2
 6 7
 8 9
 
+4
+
 
 '''
+n=int(input())
+
+left=[]
+right=[]
+
+for i in range(n):
+    x,y=map(int,input().split())
+    left.append(x)
+    right.append(y)
+
+
+L=min(right)
+R=max(left)
+
+print(L, R)
+
+result=R-L if R-L>0 else 0
+print(result)
+
+
+
--- 그리디 (easy)/22_팬덤이 넘쳐흘러(17262).py
+++ 그리디 (easy)/22_팬덤이 넘쳐흘러(17262).py
@@ -1,27 +1,80 @@
-import sys
+''''
+하다가 규칙찾기가 번거로워서 소스코드를 참고했다.
+왜 이런식으로 풀었는지는 잘 모르겠는데 
+첫번째 원소로 정렬한 뒤 맨 마지막의 첫번째 원소 - 두번째 원소로 정렬한 뒤 첫번째 순서의 두번째원소
+하면 답이 나온답.
 
-max_val = 0
-whole = set()
-for _ in range(int(sys.stdin.readline())):
-    N, M = list(map(int, sys.stdin.readline().split()))
-    check, count = False, 0
+아 그리고 
 
-    for i in range(N, M+1): # whole 안에 있으면
-        if i in whole:
-            check = True
+'''
+n=int(input())
 
-    if check == False and len(whole) == 0: # 맨처음
-        max_val = M
+left=[]
+right=[]
 
-    if check == False and len(whole) != 0: # 없으면
-        print('max : ' , max_val)
-        count += N - max_val
+for i in range(n):
+    x,y=map(int,input().split())
+    left.append(x)
+    right.append(y)
 
-    for i in range(N, M+1):
-        whole.add(i)
-        max_val = M
+L=min(right)
+R=max(left)
+
+result=R-L if R-L>0 else 0
+print(result)
 
 
-    print('cnt : ' , count, 'max_val : ' , max_val)
 
-print(count)
+# import sys
+# fans = []
+# for _ in range(int(sys.stdin.readline())):
+#     fans.append(list(map(int, sys.stdin.readline().split())))
+
+# a = sorted(fans, key=lambda x: x[0])
+# b = sorted(fans, key=lambda x: x[1])
+
+# result = a[-1][0] - b[0][1]
+
+# if result <= 0:
+#     print(0)
+# else:
+#     print(result)
+
+
+# import sys
+# minval = 100000
+# maxval = 0
+# count = 0
+# val = 0
+# cnt = 0
+# whole = set()
+
+# for _ in range(int(sys.stdin.readline())):
+#     N, M = list(map(int, sys.stdin.readline().split()))
+#     check = False
+#     for i in range(N, M+1): # whole 안에 있으면
+#         if i in whole:
+#             check = True
+#             val = min(maxval, M)
+
+#         minval = max(minval, N)
+#         maxval = min(maxval, M)
+
+#     if check == False and len(whole) == 0: # 맨처음
+#         minval = N; maxval = M; val = M
+
+#     elif check == False and len(whole) != 0: # 없으면
+#         count += N - maxval
+#         cnt += minval - val
+#         minval = N; maxval = M
+
+#     for i in range(N, M+1):
+#         whole.add(i)
+
+#     print('minval : ' , minval , ' , ' , 'maxval : ' , maxval, ', val : ' , val)
+#     print('count : ' , count)
+#     print('cnt : ' , cnt)
+
+# print(count)
+# print(whole)
+# print('cnt : ' , cnt)
