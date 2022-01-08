b629d16e16702c2444b242d5573cd64e22d7db23
dasom yun <dsy0302@gmail.com>
2022-01-08 21:55:59 +0900

    today's work

--- 그리디 (easy)/33_숫자 놀이(2777).py
+++ 그리디 (easy)/33_숫자 놀이(2777).py
@@ -1,3 +1,11 @@
 '''
 양의 정수
 '''
+
+import sys
+
+a, b = list(map(int, sys.stdin.readline().split()))
+lst = list(map(int, sys.stdin.readline().split()))
+
+for i in lst:
+    print(i-(a*b), end=' ')
