389ff5037422d5915532442e2843b90277d57840
dasom yun <dsy0302@gmail.com>
2022-02-02 21:51:12 +0900

    today's work

--- 다이내믹 프로그래밍 (easy)/44_1, 2, 3 더하기 5 (15990).py
+++ 다이내믹 프로그래밍 (easy)/44_1, 2, 3 더하기 5 (15990).py
@@ -4,3 +4,13 @@
 담에 다시 해보기 ㅠ 강의듣고..
 '''
 
+n = int(input())
+dp = [0] * (n+2)
+dp[0] = 0; dp[1] = 1; dp[2] = 1;
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+
+print(dp)
+print(dp[n])
