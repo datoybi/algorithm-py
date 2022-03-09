c9011bf21b40e327c88e842282fc9fdd3a52b61b
dasom yun <dsy0302@gmail.com>
2022-03-09 16:47:46 +0900

    정렬 스터디

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
+
 import sys
-n, S = map(int, sys.stdin.readline().split(' '))
-nums = list(map(int, sys.stdin.readline().split(' ')))
-ans = 0
 
 
-def rec_func(k, value):
-    if k == n:
-        global ans
-        if value == S:
-            ans += 1
-
-    else:
-        rec_func(k + 1, value + nums[k])
-        rec_func(k + 1, value)
+class Elem:
+    def __init__(self, name, korean, english, math):
+        self.name = name
+        self.korean = korean
+        self.english = english
+        self.math = math
 
 
-rec_func(0, 0)
-if S == 0:
-    ans -= 1
-print(ans)
+n = int(sys.stdin.readline())
+a = []
+
+for i in range(n):
+    inp = sys.stdin.readline().split()
+    inp[1:] = map(int, inp[1:])
+    a.append(Elem(*inp))
+
+a.sort(key=lambda x: (-x.korean, x.english, -x.math, x.name))
+
+names = [elem.name for elem in a]
+for name in names:
+    print(name)
