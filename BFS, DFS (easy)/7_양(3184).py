d9de17688569fa643a9d38b18809c0d40f2b4ae2
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-14 17:52:11 +0900

    dfs, bfs review

--- BFS, DFS (easy)/7_양(3184).py
+++ BFS, DFS (easy)/7_양(3184).py
@@ -43,33 +43,50 @@ print(tot_sheep, tot_wolf)
 
 
 '''
-r,c=map(int,input().split())
-g=[[{'#': 0,
-    '.':1,
-    'o':2,
-    'v':3
-    }[j]for j in input()]for i in[0]*r]
-def f(i,j):
-    q=[(i,j)]
-    x,y=0,0
-    while q:
-        i, j=q.pop()
-        if g[i][j]==2:
-            x+=1
-        elif g[i][j]==3:
-            y+=1
-        g[i][j]=0
-        for i,j in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-            if i>-1 and i<r and j>-1 and j<c and g[i][j]:q+=[[i,j]]
-    return x, y
-x, y = 0, 0
-for i in range(r):
-    for j in range(c):
-        if g[i][j]:
-            v, w = f(i,j)
-            if v > w:
-                x += v
-            else:
-                y += w
-print(x,y)
+# 두번쨰시도
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+R, C = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(R)]
+visited = [[False for _ in range(C)] for _ in range(R)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    sheep, wolf = 0, 0
+    queue = deque()
+    queue.append([x, y])
+    visited[x][y] = True
+    while queue:
+        r, c = queue.popleft()
+        if graph[r][c] == 'o':
+            sheep += 1
+        elif graph[r][c] == 'v':
+            wolf += 1
+        graph[r][c] = '#' # 이게 중요.. 또 카운트 될 가능성 있음
+        for i in range(4):
+            nx = dx[i] + r
+            ny = dy[i] + c
+            if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != '#' and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx, ny])
+    if sheep > wolf:
+        wolf = 0
+    else:
+        sheep = 0
+    return sheep, wolf
+
+t_sheep, t_wolf = 0, 0
+for i in range(R):
+    for j in range(C):
+        if graph[i][j] == 'o' or graph[i][j] == 'v':
+            sheep, wolf = bfs(i, j)
+            t_sheep += sheep
+            t_wolf += wolf
+
+print(t_sheep, t_wolf)
+
+    
 '''
--- test.py
+++ test.py
@@ -13,13 +13,43 @@ input = sys.stdin.readline
 from collections import deque
 
 R, C = map(int, input().split())
-graph = ['' for _ in range(R+1)]
-print(graph)
+graph = [list(input().rstrip()) for _ in range(R)]
+visited = [[False for _ in range(C)] for _ in range(R)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for i in range(1, R+1):
-    a = input().rstrip()
-    graph[i] = a
+def bfs(x, y):
+    sheep, wolf = 0, 0
+    queue = deque()
+    queue.append([x, y])
+    visited[x][y] = True
+    while queue:
+        r, c = queue.popleft()
+        if graph[r][c] == 'o':
+            sheep += 1
+        elif graph[r][c] == 'v':
+            wolf += 1
+        graph[r][c] = '#' # 이게 중요.. 또 카운트 될 가능성 있음
+        for i in range(4):
+            nx = dx[i] + r
+            ny = dy[i] + c
+            if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != '#' and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx, ny])
+    if sheep > wolf:
+        wolf = 0
+    else:
+        sheep = 0
+    return sheep, wolf
 
-print(graph)
-print(graph[1][1])
+t_sheep, t_wolf = 0, 0
+for i in range(R):
+    for j in range(C):
+        if graph[i][j] == 'o' or graph[i][j] == 'v':
+            sheep, wolf = bfs(i, j)
+            t_sheep += sheep
+            t_wolf += wolf
+
+print(t_sheep, t_wolf)
+
     
