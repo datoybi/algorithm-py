37a6a75530a41abcd57842386b020e0f70743b39
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-16 17:53:33 +0900

    dfs, bfs 공부

--- BFS, DFS (easy)/9_섬의 개수(4963).py
+++ BFS, DFS (easy)/9_섬의 개수(4963).py
@@ -34,6 +34,42 @@ while True:
 
 
 '''
+# 두번째 시도
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(x, y):
+    dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+    graph[x][y] = 0
+    queue = deque()
+    queue.append([x, y])
+    while queue:
+        i, j = queue.popleft()
+        for k in range(8):
+            nx, ny = dx[k] + i, dy[k] + j
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append([nx, ny])
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0 : break;
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    cnt = 0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                cnt += 1
+
+    print(cnt)
+
+
+
+
 dfs 사용
 import sys
 input = sys.stdin.readline
--- test.py
+++ test.py
@@ -1,44 +1,68 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+dx = [-2, -2, -1, 1, 2, 2, 1, -1]
+dy = [-1, 1, 2, 2, 1, -1, -2, -2]
+
+i = int(input())
+graph = [[0 for _ in range(i)] for _ in range(i)]
+print(graph)
+
+# print(visited)
+start_x, start_y = map(int, input().split())
+end_x, end_y = map(int, input().split())
+
+def bfs(x, y):
+    cnt = 999999999
+    total = 0
+    visited = [[False for _ in range(i)] for _ in range(i)]
+    queue = deque()
+    queue.append([x, y])
+    while queue:
+        x, y = queue.popleft()
+        print(x, y, total)
+        if x == end_x and y == end_y:
+            cnt = min(cnt, total)
+            print(cnt)
+        total += 1
+        for k in range(8):
+            nx, ny = dx[k] + x, dy[k] + y
+            if 0 <= nx < i and 0 <= ny < i and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx, ny])
+           
+                # 여기 넣은거 어캐 다 뺴지?
+
+bfs(start_x, start_y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''
-    w, h >= 50
-    h개의 줄 
-    1땅 0바다
-    마지막줄 0 
 
-    세로 또는 대각선으로 연결 되어 있음
-'''
-
-dx = [1, -1, -1, 0, 0, 1, 1, 1]
-dy = [-1, 0, 1, -1, 1, -1, 0, 1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-5 4
-1 0 1 0 0
-1 0 0 0 0
-1 0 1 0 1
-1 0 0 1 0
-
-3
+8
+0 0
+7 0
 '''
