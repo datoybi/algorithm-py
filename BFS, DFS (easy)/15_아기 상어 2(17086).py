c8301c1a438ebeb44440bb9a0b257078d0792a9b
dasom yun <dsy0302@gmail.com>
2022-03-21 20:17:13 +0900

    텔레포트 푸는 중

--- BFS, DFS (easy)/15_아기 상어 2(17086).py
+++ BFS, DFS (easy)/15_아기 상어 2(17086).py
@@ -34,6 +34,41 @@ print(max_val-1)
 
 
 '''
+두번쨰 시도 (혼자서 못 풀음.. 알듯 말듯..)
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+print(graph)
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+queue = deque()
+
+def bfs():
+    while queue:
+        a, b = queue.popleft()
+        for i in range(8):
+            nx, ny = dx[i] + a, dy[i] + b
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 0:
+                queue.append([nx, ny])
+                print('!' , a, b, nx, ny)
+                graph[nx][ny] = graph[a][b] + 1
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+                queue.append([i, j])
+
+bfs()
+max_val = 0
+for i in range(N):
+    max_val = max(max_val,max(graph[i]))
+
+print(max_val-1)    
+
+
 from collections import deque
 import sys
 input = sys.stdin.readline
--- test.py
+++ test.py
@@ -1,25 +1,4 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-n = int(input())
-graph = list(map(int, input().split()))
-graph.insert(0, 0)
-s = int(input())
-# print(graph)
-visited = [False for _ in range(n+1)]
-
-def bfs(v):
-    queue = deque([v])
-    visited[v] = True
-    while queue:
-        x = queue.popleft()
-        for i in [-graph[x], graph[x]]:
-            tmp = x + i
-            if 0 <= tmp <= n and not visited[tmp]:
-                queue.append(tmp)
-                visited[tmp] = True
-                            
-bfs(s)
-print(visited)
-print(visited[1:].count(True))
+N, M = map(int, input().split())
