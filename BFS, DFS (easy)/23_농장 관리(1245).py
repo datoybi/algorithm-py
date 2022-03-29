577095a2efee1a44551dd4a83f4bf09fa53d1bdb
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-29 17:52:37 +0900

    농장관리 푸는 중

--- BFS, DFS (easy)/23_농장 관리(1245).py
+++ BFS, DFS (easy)/23_농장 관리(1245).py
@@ -1,3 +1,11 @@
 '''
 
 '''
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, M = map(int, input().split())
+
+# dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
+# dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
