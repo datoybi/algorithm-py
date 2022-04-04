4bb5d1e1b51647c7acf0fe35a935d4d12d8b7e17
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-04-04 17:59:52 +0900

    N과 M시리즈 solve

--- fastCampus/brute-force/N과 M (2)(15650).py
+++ fastCampus/brute-force/N과 M (2)(15650).py
@@ -1,12 +1,11 @@
 '''
-N개 중 중복없이 M를 순서 있게 나열하기
-시간복잡도 못구하겠음..
-시간복잡도 : 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 40,320 (1억보다 작으니 1초안에 가능)
-공간복잡도 M
+    N개중 중복없이 M개를 고르기
+    시간복잡도 : 8*7 = 70
+    젤 큰값은 N=8, M=4일때
+
 
 '''
 
-# 내코드
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
@@ -14,36 +13,35 @@ selected = [0 for _ in range(M)]
 
 def solution(k):
     if k == M:
-        print(' '.join(selected))
+        for x in selected:
+            print(x, end = ' ')
+        print()
     else:
         for i in range(1, N+1):
-            if str(i) not in selected:
-                selected[k] = str(i) 
-                solution(k + 1)
-                selected[k] = 0 
+            if i > max(selected):
+                selected[k] = i
+                solution(k+1)
+                selected[k] = 0
 
 solution(0)
 
-# 강사코드 
+
+# 강사코드
 import sys
 n, m = map(int, sys.stdin.readline().split(' '))
 
 selected = [0 for _ in range(m)]
 used = [0 for _ in range(n + 1)]
-
 def rec_func(k):
     if k == m:
         for x in selected:
             sys.stdout.write(str(x) + ' ')
         sys.stdout.write('\n')
     else:
-        for cand in range(1, n + 1):
-            if used[cand]:
-                continue
+        start = 1 if k == 0 else selected[k - 1] + 1
+        for cand in range(start, n + 1):
             selected[k] = cand
-            used[cand] = 1
             rec_func(k + 1)
             selected[k] = 0
-            used[cand] = 0
 
 rec_func(0)
